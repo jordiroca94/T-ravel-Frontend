9a4d28bb115da68928ab52ec95dac716293eb82c
Jordi Roca <jordirocasoler94@gmail.com>
2022-02-28 19:44:43 +0100

    mapbox done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import * as React from 'react';
+import Map, {Marker} from 'react-map-gl';
 
-const App = () => {
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const RACT_APP_MAPBOX = "pk.eyJ1Ijoiam9yZGlyb2NhOTQiLCJhIjoiY2wwNnp0ZTZ1MDFpZTNrcDYzanhod2VnbSJ9.4qhWWAscO01UzSdinUba1Q"
+
+
+function App() {
   return (
-    <div>Hello</div>
-  )
+    <Map
+    initialViewState={{
+      latitude: 45,
+      longitude: 16,
+      zoom: 4
+    }}
+    style={{width: "100vw", height: "100vh"}}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+    mapboxAccessToken={RACT_APP_MAPBOX}
+  >
+    <Marker longitude={12.492373} latitude={41.890251} color="black" />
+  </Map>
+  );
 }
 
 export default App
